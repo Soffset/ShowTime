a468ec369539af0a82d9ac42434cc0bbd4a0e872
= <106456765+Soffset@users.noreply.github.com>
2022-11-19 18:24:55 +0100

    fixed redundand object

--- lib/models/show.api.dart
+++ lib/models/show.api.dart
@@ -10,23 +10,13 @@ class ShowApi {
 
     List data = await jsonDecode(response.body);
 
-    List temp = [];
+    List _temp = [];
 
     for (var i in data) {
-      List<String> genres = [];
-      var s = TmpShow(
-        i['name'],
-        i['image']['medium'],
-        i['id'],
-        genres,
-        ratingToString(i['rating']['average']),
-        i['averageRuntime'],
-        formatString(i['summary']),
-        i['image']['original'],);
-      temp.add(s);
+      _temp.add(i);//add each object in the json to a list
     }
 
-    return Show.showsFromSnapshot(temp);
+    return Show.showsFromSnapshot(_temp);
   }
 
   static String ratingToString(dynamic value) {
@@ -37,24 +27,6 @@ class ShowApi {
     }
   }
 
-  static String formatString(String str){
-    String formatted = str;
-    formatted = formatted.replaceAll ("<p>", "").replaceAll ("</p>", "");
-    formatted = formatted.replaceAll ("<b>", "").replaceAll ("</b>", "");
-    formatted = formatted.replaceAll ("<i>", "").replaceAll ("</i>", "");
-    return formatted;
-  }
-}
 
-class TmpShow {
-  String name;
-  String image;
-  int id;
-  List<String> genres;
-  String rating;
-  int duration;
-  String summary;
-  String fullImage;
-  TmpShow(this.name, this.image, this.id, this.genres, this.rating, this.duration, this.summary, this.fullImage);
 }
 
--- lib/models/show.dart
+++ lib/models/show.dart
@@ -4,7 +4,7 @@ class Show {
     final String name;
     final String image;
     final int id;
-    final List<String> genres;
+    //final List<String> genres;
     final String rating;
     final int duration;
     final String summary;
@@ -14,23 +14,23 @@ class Show {
       required this.name,
       required this.image,
       required this.id,
-      required this.genres,
+      //required this.genres,
       required this.rating,
       required this.duration,
       required this.summary,
       required this.fullImage});
 
-    factory Show.fromJson(TmpShow a) {
+    factory Show.fromJson(dynamic showJson) {
       return Show(
-        name: a.name,
-        image: a.image,
-        id: a.id,
-        genres: a.genres,
-        rating: a.rating,
-        duration: a.duration,
-        summary: a.summary,
-        fullImage: a.fullImage);
-
+        name: showJson['name'],
+        image: showJson['image']['medium'],
+        id: showJson['id'],
+        //genres: showJson['genres'],
+        rating: showJson['rating']['average'].toString(),
+        duration: showJson['averageRuntime'],
+        summary: formatString( showJson['summary'] ),
+        fullImage: showJson['image']['original'],
+      );
     }
 
     static List<Show> showsFromSnapshot(List snapshot) {
@@ -39,10 +39,11 @@ class Show {
       }).toList();
     }
 
-
-
-    @override
-    String toString(){
-      return 'Show {name: $name, image: $image, id: $id, genres: $genres, rating: $rating, duration: $duration, summary: $summary, fullImage: $fullImage }';
+    static String formatString(String str){
+      String formatted = str;
+      formatted = formatted.replaceAll ("<p>", "").replaceAll ("</p>", "");
+      formatted = formatted.replaceAll ("<b>", "").replaceAll ("</b>", "");
+      formatted = formatted.replaceAll ("<i>", "").replaceAll ("</i>", "");
+      return formatted;
     }
   }
