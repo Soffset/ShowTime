bc83ece4ee4b0b349b4fe3779fc25d7e3bd913fe
= <106456765+Soffset@users.noreply.github.com>
2022-11-30 22:23:20 +0100

    added searchbar

--- lib/views/widgets/home.dart
+++ lib/views/widgets/home.dart
@@ -13,6 +13,7 @@ class HomePage extends StatefulWidget {
 class HomePageState extends State<HomePage> {
   List<Show> _shows = [];
   bool _isLoading = true;
+  bool searching = false;
 
 
   @override
@@ -32,15 +33,66 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+        appBar: searching
+        ? AppBar(
+          centerTitle: true,
+          leading: const Icon(Icons.search,),
+          title: Container(
+            height: kToolbarHeight,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                  Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 6, horizontal: 3),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Theme.of(context).appBarTheme.actionsIconTheme?.color,
+                          border: OutlineInputBorder(),
+                          labelText: 'Search',
+                        ),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(
+                Icons.close,
+              ),
+              onPressed: () { setState(() {
+                searching = false;
+              }); },
+              color: Theme.of(context).appBarTheme.actionsIconTheme?.color,
+            ),
+          ],
+        )
+        : AppBar(
+          centerTitle: true,
           title: Row(
             mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
             children: const [
               Icon(Icons.movie_rounded),
               SizedBox(width: 10),
-              Text('Shows'),
+              Text('Show Time'),
             ],
           ),
+          actions: [
+            IconButton(
+              icon: const Icon(
+                Icons.search,
+              ),
+              onPressed: () { setState(() {
+                searching = true;
+              }); },
+              color: Theme.of(context).appBarTheme.actionsIconTheme?.color,
+            ),
+          ],
         ),
         body: _isLoading
               ? const Center(child: CircularProgressIndicator())
