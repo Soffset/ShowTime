1cdbecc825c513aac85827d5518b6cdd56cbf31a
= <106456765+Soffset@users.noreply.github.com>
2022-12-05 00:58:09 +0100

    working but not persistent

--- lib/views/widgets/favourites_page.dart
+++ lib/views/widgets/favourites_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:progetto_esame/models/show.dart';
+import 'package:progetto_esame/views/widgets/show_card.dart';
 
 class FavoritesPage extends StatefulWidget {
   const FavoritesPage({Key? key,
@@ -21,12 +22,16 @@ class FavoritesPage extends StatefulWidget {
 }
 
 class _FavouritesPageState extends State<FavoritesPage>{
+  List fav_shows = FavoritesPage.favorite_shows;
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
+        leading: BackButton(
+          color: Theme.of(context).appBarTheme.actionsIconTheme?.color,
+        ),
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
@@ -37,9 +42,14 @@ class _FavouritesPageState extends State<FavoritesPage>{
           ],
         ),
       ),
-      body: Container(
-        child: Text( FavoritesPage.favorite_shows.length.toString() ),
-      ),
+      body: fav_shows.isEmpty
+      ? const Center(child: Text("No favorite shows added yet!", style: TextStyle(fontSize: 20),))
+          : ListView.builder(
+      itemCount: fav_shows.length,
+      itemBuilder: (context, index) {
+      return ShowCard( show: fav_shows[index], );
+      },
+    )
     );
     throw UnimplementedError();
   }
